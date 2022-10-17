ae819cdfdb8f98c47bd66281305c36336d4c385c
Hassan ESSAJAI <Hassan.E@algoconsulting.net>
2022-10-17 11:51:42 +0100

    add coloring console and counting number of quote

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ var data = require("./quotes.json");
 
 const getRandomIndex = (length) => Math.floor(Math.random() * length+1);
 
+let count=0;
+console.log('\x1b[36m%s\x1b[0m', `Welcome in Hsn script !`);
+
 cron.schedule("*/30 * * * *", () => {
+
+  console.clear()
   const randomItem = data[getRandomIndex(data.length)];
+
   notifier.notify({
     title: randomItem.quoteAuthor,
     message: randomItem.quoteText,
   });
+  count++;
+  // counting the quotes displayed
+  console.log('\x1b[36m%s\x1b[0m', `Quote Number ${count} displayed`);
 });
